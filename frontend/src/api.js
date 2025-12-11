d824b2d235eee6fe4b853cd8306ccc0e6b28eccb
Vishwanath-C <vishwanath.kardvg@gmail.com>
2025-12-11 23:25:08 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: "http://127.0.0.1:8080",
+    // baseURL: "http://127.0.0.1:8080",
+    baseURL : import.meta.env.VITE_API_URL,
 });
 
 const token = localStorage.getItem("token");
